76d7769bfedd6096bb2132fe078d3fb2841018c3
sehar100 <Sebastian@Sebastian-NB.(none)>
2012-05-30 11:24:29 +0200

    b

--- src/main/Bombe.java
+++ src/main/Bombe.java
@@ -11,6 +11,7 @@ int reichweitel = 0;
 int reichweiter = 0;
 int reichweiteo = 0;
 int reichweiteu = 0;
+int explosion[][];
 private int bombex = 0;
 private int bombey = 0;
 private boolean sichtbar = false;
@@ -36,31 +37,36 @@ public void setSichtbar(boolean sichtbar) {
 	this.sichtbar = sichtbar;
 }
 
-/*public static void explosion() {
+public void explosion() {
 	
-    
- while (this.reichweitel <= this.bombenreichweite && Board.map[this.bombex-reichweitel][this.bombey].getTileId()%4!=1)
+	for (int i=-bombenreichweite; i< bombenreichweite; i++) {
+		for (int j=-bombenreichweite; j< bombenreichweite; j++) {
+			explosion[i][j] = 0;
+		}
+	}
+    explosion[0][0] = 1;
+ while (reichweitel <= bombenreichweite && Board.map[bombex-reichweitel][bombey].getTileId()%2 == 1)
  	{
-	 Board.map[bombex-reichweitel][bombey].setTileId(1);
+	 explosion[reichweitel][0]=1;
 	 reichweitel+=1;
 	}
- while (reichweiter <= bombenreichweite && Board.map[bombex+reichweiter][bombey].getTileId()%4!=1)
+ while (reichweiter <= bombenreichweite && Board.map[bombex+reichweiter][bombey].getTileId()%2 == 1)
 	{
-	 Board.map[bombex+reichweiter][bombey].setTileId(1);
+	 explosion[reichweiter][0]=1;
 	 reichweiter+=1;
 	}
- while (reichweiteu <= bombenreichweite && Board.map[bombex][bombey+reichweiteu].getTileId()%4!=1)
+ while (reichweiteu <= bombenreichweite && Board.map[bombex][bombey+reichweiteu].getTileId()%2 == 1)
 	{
-	 Board.map[bombex][bombey+reichweiteu].setTileId(1);
+	 explosion[0][reichweiteu]=1;
 	 reichweiteu+=1;
 	}
- while (reichweiteo <= bombenreichweite && Board.map[bombex][bombey-reichweiteo].getTileId()%4!=1)
+ while (reichweiteo <= bombenreichweite && Board.map[bombex][bombey-reichweiteo].getTileId()%2 == 1)
 	{
-	 Board.map[bombex][bombey-reichweiteo].setTileId(1);
+	 explosion[0][reichweiteo]=1;
 	 reichweiteo+=1;
 	}
 
-}*/
+}
 
 public void legeBombe() {
 	this.setBombex(Hero.getxCoord());
